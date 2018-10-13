69e92dcaf35daf0c2ecc8eec1ae7fbc3bcb2f0ed
Andrey Yatsenko <andrey_yatsenko@epam.com>
2018-10-13 14:26:48 +0400

    HOL2. Add console app

---
+++ src/EPAM.CoreWorkshop.TestConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EPAM.CoreWorkshop.TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
