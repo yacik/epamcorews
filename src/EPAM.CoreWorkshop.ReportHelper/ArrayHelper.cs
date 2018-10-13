a259ab30b47fbe790de83e7560d431be4968c93f
Andrey Yatsenko <andrey_yatsenko@epam.com>
2018-10-13 14:48:32 +0400

    HOL2. Add runtime-specific code

---
+++ src/EPAM.CoreWorkshop.ReportHelper/ArrayHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EPAM.CoreWorkshop.ReportHelper
+{
+    public static class ArrayHelper
+    {
+        public static void Fill<T>(T[] array, T value)
+        {
+#if NETCOREAPP2_0 || NETCOREAPP2_1
+            Array.Fill<T>(array, value);
+#else
+            for (var i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
+            {
+                array[i] = value;
+            }
+#endif
+        }
+    }
+}
