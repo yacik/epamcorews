aec33e17bc3c6d4e08f86668e9e4a5a9370e0963
Andrey Yatsenko <Andrey_Yatsenko@epam.com>
2021-03-29 19:14:15 +0400

    commit, squash

---
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ArrayHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPAM.CoreWorkshop.ReportHelper.Tests
+{
+    [TestClass]
+    public class ArrayHelperTests
+    {
+        [TestMethod]
+        public void FillTests()
+        {
+            var array = new int[3];
+
+            ArrayHelper.Fill(array, 5);
+
+            var expectedResult = new int[] { 5, 5, 5 };
+
+            array.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
--- src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using System.Threading;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,9 +9,7 @@ namespace EPAM.CoreWorkshop.ReportHelper.Tests
     public class ReportNameHelperTests
     {
         [TestMethod]
-        [DataRow("Hello: my world", '_', "Hello_ my world")]
-        [DataRow("Hello my %world", '_', "Hello my %world")]
-        [DataRow("Hello my /world", '_', "Hello my _world")]
+        [DynamicData("NormalizeFileNameTestData", DynamicDataSourceType.Property)]
         public void NormalizeFileNameTest(string name, char repl, string expected)
         {
             var result = ReportNameHelper.NormalizeFileName(name, repl);
@@ -22,6 +21,27 @@ namespace EPAM.CoreWorkshop.ReportHelper.Tests
         public static void Init(TestContext context)
         {
             Thread.Sleep(30 * 1000);
+        }
+
+        static object[][] NormalizeFileNameTestData
+        {
+            get
+            {
+                object[][] data = new object[][]
+                {
+                    new object[]{"Hello: my world", '_', "Hello_ my world"},
+                    new object[]{"Hello my %world", '_', "Hello my %world"},
+                    new object[]{"Hello my /world", '_', "Hello my _world"}
+                };
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    data[0][2] = "Hello: my world";
+                    data[1][2] = "Hello my %world";
+                    data[2][2] = "Hello my _world";
+                }
+                return data;
+            }
         }
     }
 }
